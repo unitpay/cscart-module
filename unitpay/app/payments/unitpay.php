8ec0813645291ba8364bd51353470631effe2f2e
Nurbek Chymbaev <n.chymbaev@unitpay.ru>
2019-09-16 15:24:35 +0300

    added payment signature

--- unitpay/app/payments/unitpay.php
+++ unitpay/app/payments/unitpay.php
@@ -5,10 +5,19 @@ $account = $order_id;
 $desc = 'Заказ #' . $order_id;
 
 $payment_url = 'https://unitpay.ru/pay/' . $processor_data['processor_params']['unitpay_public_key'];
+$secret_key = $processor_data['processor_params']['unitpay_secret_key'];
+$signature = hash('sha256', join('{up}', array(
+    $account,
+    $desc,
+    $sum,
+    $secret_key
+)));
+
 $data = array(
     'sum' => $sum,
     'account'   =>  $account,
-    'desc'  =>  $desc
+    'desc'  =>  $desc,
+    'signature' => $signature
 );
 
 fn_change_order_status($order_id, 'O');
